9a637c96c5d1b068317feae0553e14309370f498
Michael Carter <mcarter@gresham-computing.com>
2017-03-06 14:58:45 +0000

    Added gemspec and more tests.

--- test/test_fx_rate_datasource.rb
+++ test/test_fx_rate_datasource.rb
@@ -19,11 +19,24 @@ class TestFxRateDatasource < Test::Unit::TestCase
     end
   end
 
-  #TODO Parameterise this
-  def test_returns_fx_rate
+  #I would have used a parameterised test here, but didn't know how to do it in Ruby
+
+  def test_returns_fx_rate_gbp
     data = FxRateDatasource.new(TEST_FEED)
     actual_fx_rate = data.get_fx_rate(Date.new(2017, 03, 02), 'GBP')
     assert_equal(0.8556.to_s, actual_fx_rate)
+  end
+
+  def test_returns_fx_rate_usd
+    data = FxRateDatasource.new(TEST_FEED)
+    actual_fx_rate = data.get_fx_rate(Date.new(2017, 03, 02), 'USD')
+    assert_equal(1.0514.to_s, actual_fx_rate)
+  end
+
+  def test_returns_fx_rate_jpy
+    data = FxRateDatasource.new(TEST_FEED)
+    actual_fx_rate = data.get_fx_rate(Date.new(2017, 03, 02), 'JPY')
+    assert_equal(120.24.to_s, actual_fx_rate)
   end
 
   def test_invalid_date_throws_exception
